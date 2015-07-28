9833a33c7aecf3c6eabbaafe58832df49d481ef8
dexter <asaxena3079@gmail.com>
2015-07-28 18:00:04 +0530

    Shape with factory Pattern

---
+++ src/FactoryShape.java
@@ -0,0 +1,18 @@
+public class FactoryShape {
+
+    public Rectangle createRectangle(double length, double breadth)
+    {
+        return new Rectangle(length,breadth);
+    }
+
+    public Square createSquare(double length)
+    {
+        return new Square(length);
+    }
+
+    public Circle createCircle(double radius)
+    {
+        return new Circle(radius);
+    }
+
+}
--- src/MainClass.java
+++ src/MainClass.java
@@ -1,21 +1,73 @@
+import java.util.Scanner;
+
 public class MainClass {
 
     public static void main(String[] args)
     {
-        Shape rect = new Rectangle(10.0,20.0);
-        System.out.println("Rectangle");
-        System.out.println("Area is : " + rect.area());
-        System.out.println("Perimeter is : " + rect.perimeter());
+        Shape shape = null;
+        FactoryShape factoryShape = new FactoryShape();
 
-        Shape square = new Square(10.0);
-        System.out.println("\nSquare");
-        System.out.println("Area is : " + square.area());
-        System.out.println("Perimeter is : " + square.perimeter());
+        System.out.println("Select the shape you want to create : ");
+        System.out.println("1 for Rectangle");
+        System.out.println("2 for Square");
+        System.out.println("3 for RectanCirclegle");
+        System.out.println("4 for Exit");
+        System.out.println("Enter your choice");
 
-        Shape circle = new Circle(10.0);
-        System.out.println("\nCircle");
-        System.out.println("Area is : " + circle.area());
-        System.out.println("Perimeter is : " + circle.perimeter());
+        Scanner sc = new Scanner(System.in);
+        int choice = sc.nextInt();
+
+        switch (choice)
+        {
+            case 1:
+                System.out.println("You opted for Rectangle");
+                System.out.println("Enter Length : ");
+                double length = sc.nextDouble();
+                System.out.println("Enter breadth : ");
+                double breadth = sc.nextDouble();
+                shape = factoryShape.createRectangle(length,breadth);
+                break;
+
+            case 2:
+                System.out.println("You opted for Square");
+                System.out.println("Enter the length of side : ");
+                double side = sc.nextDouble();
+                shape = factoryShape.createSquare(side);
+                break;
+
+            case 3:
+                System.out.println("You opted for Circle");
+                System.out.println("Enter the radius : ");
+                double radius = sc.nextDouble();
+                shape = factoryShape.createCircle(radius);
+                break;
+
+            default:
+                System.exit(0);
+        }
+
+
+        System.out.println("What to find");
+        System.out.println("1 for Area :");
+        System.out.println("2 for Perimeter :");
+        int ch = sc.nextInt();
+        switch (ch)
+        {
+            case 1:
+                System.out.println("Area is : " + shape.area());
+                break;
+
+            case 2:
+                System.out.println("Perimeter is : " + shape.perimeter());
+                break;
+
+            default:
+                System.exit(0);
+        }
+
+
+
+
     }
 
 }
